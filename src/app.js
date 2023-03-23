3e3ecd8797c067e0200d5c3194514cf2fda8b0b8
tatahcunha <116725523+tatahcunha@users.noreply.github.com>
2023-03-23 09:15:38 -0300

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import tarefasController from "./controllers/tarefasController.js";
+import usuariosController from "./controllers/usuariosController.js";
+import cors from 'cors'
+
+const app = express()
+
+app.use(express.json())
+
+app.use(cors())
+
+    app.get("/testedeploy", (req, res) => {
+      res.status(200).send({msg: "Api sendo acessada remotamente"});
+    });
+
+tarefasController(app)
+usuariosController(app)
+
+export default app
